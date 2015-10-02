37db786ec08c1dd1efd1eceef2481ba295decac5
Koltharius <dasaji92@gmail.com>
2015-10-02 09:48:08 +0200

    Practica 2

---
+++ Practica_02/I_Ejercicio14.cpp
@@ -0,0 +1,32 @@
+/* Programa que lee un valor entero de tres digitos
+   y lo muestra por pantalla separado por tres
+   espacios en blanco.
+   Necesita: Un numero entero de 3 digitos
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+   // Declaracion de datos
+   int numero;
+   int centenas;
+   int decenas;
+   int unidades;
+
+   // Entrada de datos
+   cout << "Introduzca un numero entero de tres cifras: ";
+   cin >> numero;
+
+   // Computo de datos
+   centenas = numero / 100;
+   decenas = (numero % 100) / 10;
+   unidades = numero % 10;
+
+   // Salida de datos
+   cout << "\nEl numero es " << centenas << "   " << decenas << "   " << unidades << "\n\n";
+
+   system("pause");
+}
