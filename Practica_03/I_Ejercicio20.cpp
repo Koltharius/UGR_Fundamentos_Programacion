f2e6fcb2b7fc942f29d611d9553c85d32cf6626a
Koltharius <dasaji92@gmail.com>
2015-10-06 11:16:28 +0200

    Practica 3

---
+++ Practica_03/I_Ejercicio20.cpp
@@ -0,0 +1,21 @@
+/* Programa que lee un caracter alfabético en minuscula por teclado
+   y lo transforma en su mayuscula
+   Necesita: La introduccion de un caracter en minuscula por teclado
+   Calcula: El caracter introducido en mayuscula
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+   char caracter_minuscula;
+   char caracter_mayuscula;
+
+   cout << "Introduzca un caracter alfabetico en minuscula: ";
+   cin >> caracter_minuscula;
+
+   cout << "El caracter introducido es " << caracter_minuscula << " y su mayuscula es " << caracter_mayuscula << "\n\n"
+
+   // system("pause");
+}
