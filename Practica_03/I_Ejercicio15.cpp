dfcae4f0deddf8e3d9635446f2b55581f42df42d
Koltharius <dasaji92@gmail.com>
2015-10-06 09:59:51 +0200

    Practica_3

---
+++ Practica_03/I_Ejercicio15.cpp
@@ -0,0 +1,30 @@
+/*
+   Programa que lee por teclado las variables horas, minutos y segundos
+   y las acota a su rango correspondiente.
+   Necesita: La introduccion de tres variables enteras por parte del
+             usuario correspondientes con horas minutos y segundos.
+   Ejemplo: 10 horas 119 minutos 280 segundos lo transformaría
+            12 horas 3 minutos 40 segundos
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+   int horas;
+   int minutos;
+   int segundos;
+   int horas_resultantes;
+   int minutos_resultantes;
+   int segundos_resultantes;
+
+   cout << "Introduzca la hora: ";
+   cin >> hora;
+   cout << "\nIntroduzca los minutos: ";
+   cin >> minutos;
+   cout << "\nIntroduzca los segundos: ";
+   cin >> segundos;
+
+
+}
