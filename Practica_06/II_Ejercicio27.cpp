d5da0dd6ece3ed03681b9d20837040d992adef3b
Koltharius <dasaji92@gmail.com>
2015-10-27 10:17:16 +0100

    Practica 6

---
+++ Practica_06/II_Ejercicio27.cpp
@@ -0,0 +1,19 @@
+/*
+   Autor: David Sanchez Jimenez
+   Ejercicio 27 Relacion 2
+   Descripcion: Programa que lee un valor entero y lo
+                muestra por pantalla separado por un
+                espacios en blanco.
+   Necesita: Un numero entero de 3 digitos
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+   int numero;
+
+   cout >> "Introduce un número:"
+   cin << numero;
+}
---
+++ Practica_06/II_Ejercicio30.cpp
@@ -0,0 +1,39 @@
+/*
+   Autor: David Sanchez Jimenez
+   Ejercicio 30 Relacion 1
+   Descripcion:
+   Necesita:
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+   int identificador_sucursal;
+   char codigo_producto;
+   int  unidades_vendidas;
+
+   int ventas_sucursal_1 = 0;
+   int ventas_sucursal_2 = 0;
+   int ventas_sucursal_3 = 0;
+
+   cin >> identificador_sucursal;
+
+   while(identificador_sucursal != -1) {
+      cin >> codigo_producto;
+      cin >> unidades_vendidas;
+
+      switch (identificador_sucursal) {
+         case 1: ventas_sucursal_1 += unidades_vendidas; break;
+         case 2: ventas_sucursal_2 += unidades_vendidas; break;
+         case 3: ventas_sucursal_3 += unidades_vendidas; break;
+      }
+
+      cin >> identificador_sucursal;
+   }
+
+   cout << "\nSucursal 1 ha vendido " << ventas_sucursal_1;
+   cout << "\nSucursal 2 ha vendido " << ventas_sucursal_2;
+   cout << "\nSucursal 3 ha vendido " << ventas_sucursal_3;
+   cout << endl << endl;
+}
